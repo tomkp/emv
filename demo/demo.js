f582beb77343dec0323cbe9be2941f72cfd0bb62
tomkp <tom@tomkp.com>
2016-03-13 19:18:17 +0000

    ...

--- demo/demo.js
+++ demo/demo.js
@@ -15,21 +15,21 @@ function explore() {
     var application = emv(cardreader);
     application.selectPse()
         .then(function (response) {
-            console.info('Select PSE:\n', response.toTlvString());
+            console.info(`Select PSE:\n${response.toTlvString()}`);
             var sfi = 1;
             var record = 1;
             return application.readRecord(sfi, record);
         })
         .then(function (response) {
-            console.info('Read Record:\n', response.toTlvString());
+            console.info(`Read Record:\n${response.toTlvString()}`);
             var aid = response.find(0x4f);
-            console.info('Application ID', aid);
+            console.info(`Application ID: '${aid.toString('hex')}`);
             if (aid) {
                 return application.selectApplication(hexify.toByteArray(aid.toString('hex')));
             }
 
         }).then(function (response) {
-            console.info('Select Application:\n', response.toTlvString());
+            console.info(`Select Application:\n${response.toTlvString()}`);
 
     }).catch(function (error) {
         console.error('Error:', error, error.stack);
