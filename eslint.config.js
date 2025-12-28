d2a30ec53ecbacc5dd1a26b19b667851ffc050f7
tomkp <tom@tomkp.com>
2025-12-28 15:37:44 +0000

    Upgrade ESLint config to strictTypeChecked
    - Switch from tseslint.configs.recommended to strictTypeChecked
    - Add parserOptions for type-aware linting
    - Fix getTagName to use proper type-safe lookup
    
    Fixes #31

--- eslint.config.js
+++ eslint.config.js
@@ -4,13 +4,17 @@ import globals from 'globals';
 
 export default tseslint.config(
     js.configs.recommended,
-    ...tseslint.configs.recommended,
+    ...tseslint.configs.strictTypeChecked,
     {
         languageOptions: {
             ecmaVersion: 2022,
             sourceType: 'module',
             globals: {
                 ...globals.node,
+            },
+            parserOptions: {
+                projectService: true,
+                tsconfigRootDir: import.meta.dirname,
             },
         },
         rules: {
